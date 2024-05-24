69cb04d17519f123b0b724e673b58ba1ede925cd
Arthur Axiotes <axiotesarthur@gmail.com>
2024-05-23 22:27:50 -0300

    feat: animacao de entrada e texto dos locais e icones

--- scripts/script-galeria.js
+++ scripts/script-galeria.js
@@ -9,3 +9,21 @@ function menuMobile() {
         menu.style.transform = "scale(0)";
     }
 }
+
+const images = document.querySelectorAll(".image");
+const texts = document.querySelectorAll(".text-titulo");
+const textsReverse = document.querySelectorAll(".text-titulo-reverse");
+
+const observer = new IntersectionObserver( (elements) => {
+    elements.forEach((element) => {
+        if (element.isIntersecting){
+            element.target.classList.add("mostrar");
+        } else {
+            element.target.classList.remove("mostrar");
+        }
+    });
+});
+
+images.forEach((image) => observer.observe(image));
+texts.forEach((text) => observer.observe(text));
+textsReverse.forEach((textReverse) => observer.observe(textReverse));
