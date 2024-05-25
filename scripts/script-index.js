29515f1a3537e4dd63619b9fcaf24025714f0470
Arthur Axiotes <axiotesarthur@gmail.com>
2024-05-25 11:35:12 -0300

    feat: botao de dark mode

--- scripts/script-index.js
+++ scripts/script-index.js
@@ -9,3 +9,32 @@ function menuMobile() {
         menu.style.transform = "scale(0)";
     }
 }
+
+let isDark = document.getElementById("btn-mode");
+let mode = document.getElementById("mode");
+const spaceBtn = document.querySelector(".space-btn");
+const btnClass = document.querySelector(".btn-class");
+
+isDark = false;
+
+function trocarTema(){
+    isDark = !isDark;
+
+    if (isDark) {
+        mode.innerHTML = "Light mode"
+
+        spaceBtn.classList.add("posicao-btn-dark", "space-btn-dark");
+        btnClass.classList.add("btn-dark");
+
+        spaceBtn.classList.remove("posicao-btn-light", "space-btn-light");
+        btnClass.classList.remove("btn-light");
+    } else {
+        mode.innerHTML = "Dark mode"
+
+        spaceBtn.classList.add("posicao-btn-light", "space-btn-light");
+        btnClass.classList.add("btn-light");
+
+        spaceBtn.classList.remove("posicao-btn-dark", "space-btn-dark");
+        btnClass.classList.remove("btn-dark");
+    }
+}
