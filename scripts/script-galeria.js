740b00eeb159febcdd3a59ceba6f71dc01032886
Arthur Axiotes <axiotesarthur@gmail.com>
2024-05-26 21:23:54 -0300

    feat: dark mode na tela de galeria de imagens

--- scripts/script-galeria.js
+++ scripts/script-galeria.js
@@ -1,3 +1,18 @@
+const images = document.querySelectorAll(".image");
+const texts = document.querySelectorAll(".text-titulo");
+const textsReverse = document.querySelectorAll(".text-titulo-reverse");
+const checkbox = document.getElementById("check-mode");
+const labelColor = document.querySelector(".label");
+const btnLabelColor = document.querySelector(".btn-label");
+const header = document.querySelector(".header");
+const body = document.body;
+const footer = document.querySelector(".footer");
+const titulo = document.querySelector("#titulo");
+const subtitulo = document.querySelectorAll(".subtitulo");
+const tituloSessao = document.querySelectorAll(".titulo-sessao");
+const text = document.querySelectorAll(".text");
+const feedback = document.querySelector("#feedback");
+
 function menuMobile() {
     let menu = document.getElementById('menu-mobile');
 
@@ -9,10 +24,6 @@ function menuMobile() {
         menu.style.transform = "scale(0)";
     }
 }
-
-const images = document.querySelectorAll(".image");
-const texts = document.querySelectorAll(".text-titulo");
-const textsReverse = document.querySelectorAll(".text-titulo-reverse");
 
 const observer = new IntersectionObserver( (elements) => {
     elements.forEach((element) => {
@@ -28,10 +39,6 @@ images.forEach((image) => observer.observe(image));
 texts.forEach((text) => observer.observe(text));
 textsReverse.forEach((textReverse) => observer.observe(textReverse));
 
-const checkbox = document.getElementById("check-mode");
-const labelColor = document.querySelector(".label");
-const btnLabelColor = document.querySelector(".btn-label");
-
 function toggleMode() {
     labelColor.classList.toggle("label-dark");
     labelColor.classList.toggle("label-light");
@@ -44,6 +51,30 @@ function toggleMode() {
 
     body.classList.toggle("back-color-light");
     body.classList.toggle("back-color-dark");
+
+    footer.classList.toggle("footer-dark");
+    footer.classList.toggle("footer-light");
+
+    titulo.classList.toggle("letras-dark");
+    titulo.classList.toggle("letras-light");
+
+    subtitulo.forEach((element) => {
+        element.classList.toggle("letras-light");
+        element.classList.toggle("letras-dark");
+    });
+
+    text.forEach((element) => {
+        element.classList.toggle("letras-light");
+        element.classList.toggle("letras-dark");
+    });
+
+    tituloSessao.forEach((element) => {
+        element.classList.toggle("letras-light");
+        element.classList.toggle("letras-dark");
+    });
+
+    feedback.classList.toggle("letras-light");
+    feedback.classList.toggle("letras-dark");
 }
 
 function addMode() {
